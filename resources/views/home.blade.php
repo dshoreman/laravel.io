b67bcc5ca48877d12a62481185dd3675b6104b06
Joe Dixon <hello@joedixon.co.uk>
2021-05-04 21:30:47 +0100

    Add threads section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,26 +39,28 @@
     <!-- /Banner ad -->
 
     <!-- Search -->
-    <section class="bg-lio-500 text-white transform -skew-y-1 mb-32">
-        <div class="container mx-auto transform skew-y-1">
-            <div class="flex flex-col items-center py-20 text-center">
-                <div class="w-2/5">
-                    <div class="mb-8">
-                        <h2 class="text-4xl font-bold mb-3">Looking for a solution?</h2>
-                        <p class="text-xl opacity-80">Search the forum for the answer to your question</p>
-                    </div>
-
-                    <div class="mb-10">
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                <x-heroicon-o-search class="w-4 h-4 text-gray-900" />
-                            </div>
-                            <input type="search" placeholder="Search here for threads" class="p-4 pl-10 text-gray-600 rounded w-full" />
+    <section class="my-16">
+        <div class="bg-lio-500 text-white transform -skew-y-1">
+            <div class="container mx-auto transform skew-y-1">
+                <div class="flex flex-col items-center py-20 text-center">
+                    <div class="w-2/5">
+                        <div class="mb-8">
+                            <h2 class="text-4xl font-bold mb-3">Looking for a solution?</h2>
+                            <p class="text-xl opacity-80">Search the forum for the answer to your question</p>
                         </div>
-                    </div>
 
-                    <div class="text-lg">
-                        <p>Can't find what you're looking for? <a href="" class="border-b border-white pb-1">Create a new thread</a></p>
+                        <div class="mb-10">
+                            <div class="relative">
+                                <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                    <x-heroicon-o-search class="w-4 h-4 text-gray-900" />
+                                </div>
+                                <input type="search" placeholder="Search here for threads" class="p-4 pl-10 text-gray-600 rounded w-full" />
+                            </div>
+                        </div>
+
+                        <div class="text-lg">
+                            <p>Can't find what you're looking for? <a href="" class="border-b border-white pb-1">Create a new thread</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -67,30 +69,98 @@
     <!-- /Search -->
 
     <!-- Help others -->
-    <section>
+    <section class="my-16 container mx-auto">
         <div>
-            <h2>Or you can help others</h2>
-            <p>By joining our platform, you can take a look at the latest unresolved threads</p>
+            <div class="flex items-center mb-16">
+                <h2 class="text-4xl font-bold text-gray-900 w-1/2">Or you can help others</h2>
+                <p class="w-1/2 text-gray-800 text-lg">By joining our platform, you can take a<br> look at the latest unresolved threads</p>
+            </div>
 
-            <div>
-                <div>
-                    <div>
-                        <img class="w-6 h-6 rounded-full mx-4 my-2" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=3iakaAt3nn&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="Tim Cook" />
-                        <span>Tim Cook</span>
+            <div class="flex mb-10">
+                <div class="flex flex-col rounded w-1/3 shadow-lg p-5 mr-6">
+                    <div class="flex justify-between mb-2.5">
+                        <div class="flex items-center">
+                            <img class="w-8 h-8 rounded-full mr-2" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=3iakaAt3nn&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="Tim Cook" />
+                            <span class="font-heading text-sm text-black">Tim Cook</span>
+                        </div>
+
+                        <div>
+                            <span class="text-sm text-gray-600">
+                                4 hours ago
+                            </span>
+                        </div>
                     </div>
 
-                    <div>
-                        4 hours ago
+                    <div class="flex-auto flex flex-col justify-between">
+                        <div>
+                            <h3 class="text-gray-900 text-2xl mb-2 leading-8">Cloudinary Laravel SDK</h3>
+                            <p class="text-gray-800 text-base leading-7">Hey, Does anyone have experience with cloudinary-laravel-sdk? Any examples on CRUD operations on thi...</p>
+                        </div>
+                        <a href="" class="flex items-center text-base text-gray-300">
+                            <span class="text-gray-700 mr-1">Open thread</span>
+                            <x-heroicon-s-arrow-right class="w-4 h-4 fill-current" />
+                        </a>
                     </div>
                 </div>
 
-                <div>
-                    <h3>Cloudinary Laravel SDK</h3>
-                    <p>Hey, Does anyone have experience with cloudinary-laravel-sdk? Any examples on CRUD operations on thi...</p>
+                <div class="flex flex-col rounded w-1/3 shadow-lg p-5 mr-6">
+                    <div class="flex justify-between mb-2.5">
+                        <div class="flex items-center">
+                            <img class="w-8 h-8 rounded-full mr-2" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=3iakaAt3nn&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="Tim Cook" />
+                            <span class="font-heading text-sm text-black">Tim Cook</span>
+                        </div>
+
+                        <div>
+                            <span class="text-sm text-gray-600">
+                                4 hours ago
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="flex-auto flex flex-col justify-between">
+                        <div>
+                            <h3 class="text-gray-900 text-2xl mb-2 leading-8">Cloudinary Laravel SDK</h3>
+                            <p class="text-gray-800 text-base leading-7">Hey, Does anyone have experience with cloudinary-laravel-sdk...</p>
+                        </div>
+                        <a href="" class="flex items-center text-base text-gray-300">
+                            <span class="text-gray-700 mr-1">Open thread</span>
+                            <x-heroicon-s-arrow-right class="w-4 h-4 fill-current" />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="flex flex-col rounded w-1/3 shadow-lg p-5 mr-6">
+                    <div class="flex justify-between mb-2.5">
+                        <div class="flex items-center">
+                            <img class="w-8 h-8 rounded-full mr-2" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=3iakaAt3nn&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="Tim Cook" />
+                            <span class="font-heading text-sm text-black">Tim Cook</span>
+                        </div>
+
+                        <div>
+                            <span class="text-sm text-gray-600">
+                                4 hours ago
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="flex-auto flex flex-col justify-between">
+                        <div>
+                            <h3 class="text-gray-900 text-2xl mb-2 leading-8">Cloudinary Laravel SDK</h3>
+                            <p class="text-gray-800 text-base leading-7">Hey, Does anyone have experience with cloudinary-laravel-sdk...</p>
+                        </div>
+                        <a href="" class="flex items-center text-base text-gray-300">
+                            <span class="text-gray-700 mr-1">Open thread</span>
+                            <x-heroicon-s-arrow-right class="w-4 h-4 fill-current" />
+                        </a>
+                    </div>
                 </div>
             </div>
 
-            <a href="">View all threads</a>
+            <div class="flex justify-center">
+                <x-buttons.primary-cta href="">
+                    View all threads
+                </x-buttons.primary-cta>
+            </div>
         </div>
     </section>
     <!-- /Help others -->
