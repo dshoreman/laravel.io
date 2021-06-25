5f3d82a24614344e3578d4f77e6a55e03e9113f3
Joe Dixon <hello@joedixon.co.uk>
2021-06-25 15:17:32 +0100

    Add hover state to members

--- resources/views/components/community-members.blade.php
+++ resources/views/components/community-members.blade.php
@@ -1,10 +1,10 @@
-<div class="flex flex-col w-full">
+<div class="flex flex-col w-full" x-data="{ active: false }">
     <div class="flex" style="margin-left: 134px;">
         @foreach ($members->first() as $member)
-            <div x-data="{ open: false }" class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2">
+            <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
                 <x-stat-popout 
                     x-cloak 
-                    x-show="open" 
+                    x-show="active == '{{ $member->id }}'" 
                     class="w-64 absolute -mt-40 -ml-20"
                     :user="$member"
                 />
@@ -12,8 +12,9 @@
                 <x-avatar 
                     :user="$member" 
                     class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                    x-on:mouseover="open = true"
-                    x-on:mouseout="open = false"
+                    x-bind:class="{ 'opacity-50': active && active != '{{ $member->id }}' }"
+                    x-on:mouseover="active = {{ $member->id }}"
+                    x-on:mouseout="active = false"
                 />
             </div>
         @endforeach
@@ -21,10 +22,10 @@
 
     <div class="flex" style="margin-left: 71px;">
         @foreach ($members[1] as $member)
-            <div x-data="{ open: false }" class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2">
+            <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
                 <x-stat-popout 
                     x-cloak 
-                    x-show="open" 
+                    x-show="active == '{{ $member->id }}'" 
                     class="w-64 absolute -mt-40 -ml-20"
                     :user="$member"
                 />
@@ -32,8 +33,9 @@
                 <x-avatar 
                     :user="$member" 
                     class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                    x-on:mouseover="open = true"
-                    x-on:mouseout="open = false"
+                    x-bind:class="{ 'opacity-50': active && active != '{{ $member->id }}' }"
+                    x-on:mouseover="active = {{ $member->id }}"
+                    x-on:mouseout="active = false"
                 />
             </div>
         @endforeach
@@ -41,10 +43,10 @@
 
     <div class="flex">
         @foreach ($members[2] as $member)
-            <div x-data="{ open: false }" class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2">
+            <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
                 <x-stat-popout 
                     x-cloak 
-                    x-show="open" 
+                    x-show="active == '{{ $member->id }}'" 
                     class="w-64 absolute -mt-40 -ml-20"
                     :user="$member"
                 />
@@ -52,8 +54,9 @@
                 <x-avatar 
                     :user="$member" 
                     class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                    x-on:mouseover="open = true"
-                    x-on:mouseout="open = false"
+                    x-bind:class="{ 'opacity-50': active && active != '{{ $member->id }}' }"
+                    x-on:mouseover="active = {{ $member->id }}"
+                    x-on:mouseout="active = false"
                 />
             </div>
         @endforeach
@@ -61,10 +64,10 @@
 
     <div class="flex" style="margin-left: 71px;">
         @foreach ($members[3] as $member)
-            <div x-data="{ open: false }" class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2">
+            <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
                 <x-stat-popout 
                     x-cloak 
-                    x-show="open" 
+                    x-show="active == '{{ $member->id }}'" 
                     class="w-64 absolute -mt-40 -ml-20"
                     :user="$member"
                 />
@@ -72,8 +75,9 @@
                 <x-avatar 
                     :user="$member" 
                     class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                    x-on:mouseover="open = true"
-                    x-on:mouseout="open = false"
+                    x-bind:class="{ 'opacity-50': active && active != '{{ $member->id }}' }"
+                    x-on:mouseover="active = {{ $member->id }}"
+                    x-on:mouseout="active = false"
                 />
             </div>
         @endforeach
@@ -81,19 +85,20 @@
 
     <div class="flex" style="margin-left: 134px;">
         @foreach ($members[4] as $member)
-            <div x-data="{ open: false }" class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2">
+            <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
                 <x-stat-popout 
                     x-cloak 
-                    x-show="open" 
+                    x-show="active == '{{ $member->id }}'" 
                     class="w-64 absolute -mt-40 -ml-20"
                     :user="$member"
                 />
-                
+
                 <x-avatar 
                     :user="$member" 
                     class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                    x-on:mouseover="open = true"
-                    x-on:mouseout="open = false"
+                    x-bind:class="{ 'opacity-50': active && active != '{{ $member->id }}' }"
+                    x-on:mouseover="active = {{ $member->id }}"
+                    x-on:mouseout="active = false"
                 />
             </div>
         @endforeach
